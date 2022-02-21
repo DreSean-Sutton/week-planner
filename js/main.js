2f7aa2bebac9f94a96102e15a3b226f6b013a0c5
DreSean-Sutton <dreseansutton@gmail.com>
2022-02-20 17:12:16 -0800

    Work in progress

--- js/main.js
+++ js/main.js
@@ -1,12 +1,33 @@
+var modalIsOpen = false;
+
 var $addEntryButton = document.querySelector('.add-entry-button');
 var $form = document.querySelector('form');
 var $modal = document.querySelector('#modal');
 var $overlay = document.querySelector('.overlay');
-
-// $form.addEventListener('submit', event => {
-
-// });
-$addEntryButton.addEventListener('click', event => {
-  $modal.classList.remove('hidden');
-  $overlay.classList.remove('hidden');
+$form.addEventListener('submit', event => {
+  event.preventDefault();
+  var formValues = {
+    day: $form.elements.week.value,
+    time: $form.elements.time.value,
+    description: $form.elements.description.value
+  };
+  console.log('formValues result', formValues);
+  $form.reset();
+  openModal();
 });
+
+$addEntryButton.addEventListener('click', event => {
+  openModal();
+});
+
+var openModal = () => {
+  if (modalIsOpen) {
+    $modal.classList.add('hidden');
+    $overlay.classList.add('hidden');
+    modalIsOpen = false;
+  } else {
+    $modal.classList.remove('hidden');
+    $overlay.classList.remove('hidden');
+    modalIsOpen = true;
+  }
+};
